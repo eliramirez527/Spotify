b908488d72f32365c8c8bdf0633c14addbb9438b
eliramirez527 <118145292+eliramirez527@users.noreply.github.com>
2024-05-02 21:33:26 -0500

    camb2

--- src/main/java/com/example/spotify/RegistroActivity.java
+++ src/main/java/com/example/spotify/RegistroActivity.java
@@ -11,7 +11,6 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
